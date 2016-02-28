ae08231cfedc0e348e07aa8a2aa3b85167f75a60
Izaak Prats <izaak.prats@gmail.com>
2016-02-27 21:38:45 -0500

    trying tomake it work

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,4 +14,19 @@ class ProjectsController < ApplicationController
       @project = current_user.projects.new
     end
   end
+
+  def create
+    @project = current_user.projects.build(project_params)
+    if @project.save
+      flash[:success] = "Project created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
+
+    private
+
+   def project_params
+     params.require(:project)
+   end
 end
